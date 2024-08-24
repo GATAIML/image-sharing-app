6367cce2a16389d554b03b04387c4c37f3b1ff58
GATAIML <145026689+GATAIML@users.noreply.github.com>
2024-08-24 16:29:33 +0000

    Social Link Change

--- src/App.js
+++ src/App.js
@@ -53,15 +53,15 @@ const App = () => {
               <div className="container mx-auto flex items-center justify-between">
                 <span className="text-gray-800">
                   Created by{" "}
-                  <a href="https://www.linkedin.com/in/bhuvanar/" className="font-bold text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">
+                  <a href="https://www.linkedin.com/in/bhuvan-a-r/" className="font-bold text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">
                     Bhuvan AR
                   </a>
                 </span>
                 <div className="space-x-4">
-                  <a href="https://www.linkedin.com/in/bhuvanar/" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-blue-600">
+                  <a href="https://www.linkedin.com/in/bhuvan-a-r/" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-blue-600">
                     <FontAwesomeIcon icon={faLinkedin} size="lg" />
                   </a>
-                  <a href="https://github.com/bhuvanar" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-gray-600">
+                  <a href="https://github.com/bhuvan-a-r" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-gray-600">
                     <FontAwesomeIcon icon={faGithub} size="lg" />
                   </a>
                 </div>
